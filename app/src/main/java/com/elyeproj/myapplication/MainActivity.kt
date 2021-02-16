7a33706aee8992d8f0ffd7b51c890931e5641b0a
Elisha Lye <elisha.lye@rea-group.com>
2021-02-16 21:24:36 +1100

    Update version

--- app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
+++ app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
@@ -3,9 +3,7 @@ package com.elyeproj.myapplication
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.View
 import android.widget.RadioButton
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
